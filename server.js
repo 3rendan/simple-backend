2ca97237f6443387c5464652eff763d64e6a6d50
Brendan Ryan <brendan.ryan@pm.me>
2023-07-27 22:22:32 -0400

    072723

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const axios = require('axios')
 require('dotenv').config()
 
 const config = {
-  headers: { Authorization: `Token token=${process.env.API_TOKEN}`}
+  headers: { Authorization: `Token token=fd2ae3c0998894af046caf8961770dc7`}
 }
 
 app.use(bodyParser.json())
 
 app.get('/podcasts', async (req, res) => {
   try {
-    const response = await axios.get(`${process.env.API_URL}/podcasts`, config);
+    const response = await axios.get(`https://cms.megaphone.fm/api/networks/126216ae-056d-11ee-9189-676656f0e82d/podcasts`, config);
     res.status(200).json( response.data );
   } catch (error) {
     res.json(error);
