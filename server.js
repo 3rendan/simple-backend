ef53e8c8375a698a43047e85840aa7088d2e4d76
Brendan Ryan <brendan.ryan@pm.me>
2023-10-16 14:18:39 -0400

    Set up to query the api gateway through localhost

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.use(bodyParser.json())
 app.use(cors())
 app.get('/podcasts', async (req, res) => {
   try {
-    const response = await axios.get(`https://cms.megaphone.fm/api/networks/126216ae-056d-11ee-9189-676656f0e82d/podcasts`, config)
+    const response = await axios.get(`https://pt95erup1m.execute-api.us-east-1.amazonaws.com/prod/podcasts`)
     res.status(200).json( response.data )
   } catch (error) {
     console.log(error)
   }
 })
 
-app.get(`/AE-episodes`, async (req, res) => {
+app.get(`/ae-episodes`, async (req, res) => {
   try {
-    const response = await axios.get(`https://cms.megaphone.fm/api/networks/126216ae-056d-11ee-9189-676656f0e82d/podcasts/06c45fb2-1c17-11ee-9eb6-ff2ec7065f86/episodes`, config)
+    const response = await axios.get(`https://pt95erup1m.execute-api.us-east-1.amazonaws.com/prod/ae-episodes`)
     res.status(200).json( response.data )
   } catch (error) {
     console.log(error)
@@ -31,7 +31,7 @@ app.get(`/AE-episodes`, async (req, res) => {
 
 app.get(`/gravy-episodes`, async (req, res) => {
   try {
-    const response = await axios.get(`https://cms.megaphone.fm/api/networks/126216ae-056d-11ee-9189-676656f0e82d/podcasts/f5054664-21a3-11ee-8031-c7f78aa2421e/episodes`, config)
+    const response = await axios.get(`https://pt95erup1m.execute-api.us-east-1.amazonaws.com/prod/gravy-episodes`)
     res.status(200).json( response.data )
   } catch (error) {
     console.log(error)
@@ -40,13 +40,13 @@ app.get(`/gravy-episodes`, async (req, res) => {
 
 app.get(`/food-episodes`, async (req, res) => {
   try {
-    const response = await axios.get(`https://cms.megaphone.fm/api/networks/126216ae-056d-11ee-9189-676656f0e82d/podcasts/299729f4-056d-11ee-974e-f7d1b7547a45/episodes`, config)
+    const response = await axios.get(`https://pt95erup1m.execute-api.us-east-1.amazonaws.com/prod/food-episodes`)
     res.status(200).json( response.data )
   } catch (error) {
     console.log(error)
   }
 })
 
-app.listen(80, () => {
-  console.log('listening on port 80')
+app.listen(8080, () => {
+  console.log('listening on port 8080')
 })
